b6cecd03bc7ebb04b46bd2a7e062d994bd19d9cf
Dancing bull <72798082+upscaspirantmzr@users.noreply.github.com>
2025-10-13 13:49:01 +0530

    Create firebaseconfig.js

---
+++ backend/secured/firebaseconfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBSZksx94eCTBHC0XKwnq9ubZn9oKD4AGE",
+  authDomain: "aus-updates.firebaseapp.com",
+  databaseURL: "https://aus-updates-default-rtdb.firebaseio.com",
+  projectId: "aus-updates",
+  storageBucket: "aus-updates.appspot.com",
+  messagingSenderId: "1002850804557",
+  appId: "1:1002850804557:web:140825c35721d18e5e8d04",
+  measurementId: "G-QD9LDX9CTQ"
+};
